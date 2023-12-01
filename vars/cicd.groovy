1f3cd24710b08f2f5a272be3fabb3ccb63ce044c
sudarshansw7 <142533426+sudarshansw7@users.noreply.github.com>
2023-12-01 16:29:30 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -2,3 +2,15 @@ def gitdownload(repo)
 {
   git "https://github.com/sudarshansw7/${repo}.git"
 }
+def build()
+{
+  sh "mvn package"
+}
+def deploy(jobname,ip,appname)
+{
+   sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${appname}.war"
+}
+def runselenium(jobname)
+{
+  sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+}
